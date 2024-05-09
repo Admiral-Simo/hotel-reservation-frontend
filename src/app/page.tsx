c0e273f6a9759f03073dd67cb9013fbf1e8aade7
Admiral-Simo <admiralsimo16@gmail.com>
2024-05-09 21:46:18 +0100

    make a link for auth redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-yellow-500 text-black flex justify-center items-center">
-      <h1 className="font-bold text-5xl">To Make This Page</h1>
+      <div>
+        <h1 className="text-5xl text-gray-800">To Make This Page</h1>
+        <Link
+          className="font-bold text-5xl underline text-gray-900"
+          href="/auth/signin"
+        >
+          Authenticate
+        </Link>
+      </div>
     </div>
   );
 }
