54753ca19eb845935ebdddabee0048e39e701f9d
Admiral-Simo <admiralsimo16@gmail.com>
2024-05-20 12:17:05 +0100

    experiment with useUser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
+"use client";
 import Link from "next/link";
+import { useUser } from "@/context/UserContext";
 
 export default function Home() {
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <div className="min-h-screen bg-yellow-500 text-black flex justify-center items-center">
       <div>
         <h1 className="text-5xl text-gray-800">To Make This Page</h1>
-        <Link
-          className="font-bold text-5xl underline text-gray-900"
-          href="/auth/signin"
-        >
-          Authenticate
-        </Link>
+
+        {user ? (
+          JSON.stringify(user)
+        ) : (
+          <Link
+            className="font-bold text-5xl underline text-gray-900"
+            href="/auth/signin"
+          >
+            Authenticate
+          </Link>
+        )}
       </div>
     </div>
   );
